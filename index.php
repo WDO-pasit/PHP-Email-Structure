214a0b3dc1e98de9ac60fc063e41cd8917a5bdbf
Ohm WebDev <ohmdevweb@gmail.com>
2025-06-18 07:45:39 +0700

    php website with header reuse support php project and support php server

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php define('BASE_URL', 'http://localhost/Git/Pasit/website_php/'); ?>
+<?php
+$pageConfig = [
+    "title" => "pasit workshop",
+    "description" => "Dev mode in thailand",
+    "keywords" => "Just use my work to your space",
+    "canonical" => "https://webdevohm.com/custom",
+    "og_image" => "https://webdevohm.com/asset/img/Normal/custom.webp",
+    "og_url" => "https://webdevohm.com/custom",
+    "og_type" => "website"
+];
+?>
+<?php include(__DIR__ . '/component/header.php'); ?>
+  <main>
+        <section>
+            <div class="home-coming">
+                <h1>Dev Pasit</h1>
+                
+
+            </div>
+        </section>
+    </main>
+<?php include(__DIR__ . '/component/footer.php'); ?>
---
+++ myproject.php
@@ -0,0 +1,23 @@
+<?php define('BASE_URL', 'http://localhost/Git/Pasit/website_php/'); ?>
+<?php
+$pageConfig = [
+    "title" => "หน้าหลักสั่งทำเว็บ | Web Dev Ohm",
+    "description" => "ระบบสั่งทำ ในเว็บไซต์ Web Dev OHM ระบบสั่งทำคือการเปิดโอกาสให้ผู้ใช้บริการสามารถแสดงความต้องการได้อย่างเต็มที่ แดชบอร์ดติตามงาน ไม่ทำด้วยแแต่ให้ออกแบบระบบให้เริ่มต้น15000 landingweb multiweb  SEO, URL Friendly, Sitemap.xml blacklink ครบชุด 5 Page เช่น หน้าแรก สินค้า เกี่ยวกับเรา ติดต่อ รีวิว ส่งมอบ Source Code และสิทธิ์ทั้งหมดหลังชำระเงิน ",
+    "keywords" => "สั่งทำ Google หรือ OAuth อื่น ๆ SEO, URL Friendly, Sitemap.xml",
+    "canonical" => "https://webdevohm.com/custom",
+    "og_image" => "https://webdevohm.com/asset/img/Normal/custom.webp",
+    "og_url" => "https://webdevohm.com/custom",
+    "og_type" => "website"
+];
+?>
+<?php include(__DIR__ . '/component/header.php'); ?>
+  <main>
+        <section>
+            <div class="home-coming">
+                <h1>My work</h1>
+                
+
+            </div>
+        </section>
+    </main>
+<?php include(__DIR__ . '/component/footer.php'); ?>
